a1dabdc49020cccc3031833681b3e081ea950a74
Amr Gamal <amrgamal.agm@gmail.com>
2026-09-17 14:25:35 +0300

    feat: add GreetingHeader component to Home screen

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,10 +1,20 @@
-import { colors } from "@/src/constants/colors";
-import { Text, View } from "react-native";
+import GreetingHeader from "@/src/components/GreetingHeader";
+import useTheme from "@/src/hooks/useTheme";
+import { View } from "react-native";
+
 export default function Home() {
+  const { colors, fonts, fontSizes, spacing } = useTheme();
   return (
     <>
-      <View>
-        <Text style={{ color: colors.light.text }}>Good Morning, Alex</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "baseline",
+          justifyContent: "space-between",
+          padding: spacing.lg,
+        }}
+      >
+        <GreetingHeader name="Alex" />
       </View>
     </>
   );
---
+++ src/components/GreetingHeader.tsx
@@ -0,0 +1,23 @@
+import { Text, View } from "react-native";
+import useTheme from "../hooks/useTheme";
+
+export default function GreetingHeader({ name }: { name: string }) {
+  const { colors, fonts, fontSizes, spacing } = useTheme();
+  return (
+    <View>
+      <Text
+        style={{
+          color: colors.text,
+          fontFamily: fonts.heading,
+          fontSize: fontSizes.xxl,
+        }}
+      >
+        Good Morning, {"\n"}
+        {name}
+      </Text>
+      <Text style={{ padding: spacing.sm, color: colors.textMuted }}>
+        What's on your mind today?
+      </Text>
+    </View>
+  );
+}
