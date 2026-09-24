40401221d2b79b2e38df4ca8eca04856edc96901
Amr Gamal <amrgamal.agm@gmail.com>
2026-09-24 19:58:55 +0300

    refactor: move new entry logic to dedicated screen

--- src/app/(app)/diary/[id]/new-entry.tsx
+++ src/app/(app)/diary/[id]/new-entry.tsx
@@ -2,13 +2,21 @@ import Editor from "@/src/components/Editor";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function NewEntryScreen() {
   const { id } = useLocalSearchParams();
   const router = useRouter();
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Text>This is editor</Text>
-      <Editor diaryId={id} onBack={router.back} />
+      <Editor
+        diaryId={Number(id)}
+        entryToEdit={null}
+        initialReadOnly={false}
+        onBack={() => {
+          router.back();
+        }}
+      />
     </SafeAreaView>
   );
 }
