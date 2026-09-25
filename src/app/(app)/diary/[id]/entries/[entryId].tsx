eb94b27cb86d79780f19c8797cd8a8b05ac512e1
Amr Gamal <amrgamal.agm@gmail.com>
2026-09-25 03:21:57 +0300

    fix: show entry body when editing

--- src/app/(app)/diary/[id]/entries/[entryId].tsx
+++ src/app/(app)/diary/[id]/entries/[entryId].tsx
@@ -2,26 +2,33 @@ import Editor from "@/src/components/Editor";
 import { getEntryById, saveEntry } from "@/src/db/entries";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function EntryEditScreen() {
   const { id, entryId } = useLocalSearchParams();
-  const [entry, setEntry] = useState()
+  const [entry, setEntry] = useState();
   const diaryId = Number(id);
-  const entryToEdit = getEntryById(Number(entryId));
   const router = useRouter();
+
+  useEffect(() => {
+    const loadEntry = async () => {
+      const result = await getEntryById(Number(entryId));
+      console.log(result);
+      setEntry(result);
+    };
+    loadEntry();
+  }, [entryId]);
 
   const handleSave = (title: string, body: string) => {
     saveEntry(diaryId, title, body);
     router.back();
   };
-  
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Editor
-        initialTitle={entryToEdit?.title}
-        initialBody={entryToEdit?.body}
-        initialDate={entryToEdit?.created_at}
+        initialTitle={entry?.title}
+        initialBody={entry?.body}
+        initialDate={entry?.created_at}
         initialReadOnly={true}
         onSave={(title, body) => handleSave(title, body)}
         onBack={() => {
